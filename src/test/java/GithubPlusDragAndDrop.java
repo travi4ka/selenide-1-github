be0f41951dbd42b58b8e031121c3c8b36dfeb998
travi4ka <74044177+travi4ka@users.noreply.github.com>
2022-12-27 15:12:41 +0300

    123

--- src/test/java/GithubPlusDragAndDrop.java
+++ src/test/java/GithubPlusDragAndDrop.java
@@ -1,23 +1,21 @@
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-
 public class GithubPlusDragAndDrop {
     @Test
-    void githubEnterprisePage(){
-        open("https://github.com/");
-        $(byText("Solutions")).hover();
-        $(byText("Enterprise")).click();
-        $(byText("Build like the best")).shouldBe(visible);
-    }
-
-    @Test
-    void drugDrop(){
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDropTo("#column-b");
-        $("#column-a header").shouldHave(text("B"));
-        $("#column-b header").shouldHave(text("A"));
+    void softAssertionsIsOnTheWikiPage(){
+//        - Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+//        - Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+//        - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(byText("Show 2 more pages…")).click();
+        $(".wiki-pages-box button").click();
+        $(".wiki-pages-box").shouldHave(text("SoftAssertions"));
+//        - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"))
+                .sibling(0).$("pre").shouldBe(visible);
     }
 }
